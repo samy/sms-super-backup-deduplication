b11c8fadf0b208404b5f7a80a4eb2baefd1d6f5d
Samy Rabih <contact@lahaut.info>
2013-10-20 21:52:37 +0200

    Upload management + code cleaning

--- js/sms.js
+++ js/sms.js
@@ -11,29 +11,76 @@ $(document).ready(function() {
         }
       );    
 	});
+	
+	$(document).on('dragenter', '#dropfile', function() {
+            $(this).css('border', '3px dashed red');
+            return false;
+	});
+	 
+	$(document).on('dragover', '#dropfile', function(e){
+				e.preventDefault();
+				e.stopPropagation();
+				$(this).css('border', '3px dashed red');
+				return false;
+	});
+	 
+	$(document).on('dragleave', '#dropfile', function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				$(this).css('border', '3px dashed #BBBBBB');
+				return false;
+	});
+	$(document).on('drop', '#dropfile', function(e) {
+		if(e.originalEvent.dataTransfer){
+			if(e.originalEvent.dataTransfer.files.length) {
+				// Stop the propagation of the event
+				e.preventDefault();
+				e.stopPropagation();
+				$(this).css('border', '3px dashed green');
+				// Main function to upload
+				upload(e.originalEvent.dataTransfer.files);
+			}  
+		}
+		else {
+				   $(this).css('border', '3px dashed #BBBBBB');
+		}
+		return false;
+	});
 });
-function displaySMS(xmlSource) {
+function deduplicateSMS(xmlSource) {
 	var ids = new Array();
 	var duplicates= 0;
+	var total= 0;
 	
 	$(xmlSource).find('allsms sms').each(function(index,e) {
 		if (ids.indexOf($(this).attr('address')+$(this).attr('date'))==-1) {
+			total+=1;
 			ids.push($(this).attr('address')+$(this).attr('date'));
 		} else {
 			
 			duplicates+=1;
 			$(this).remove();
-			console.log($(this));
 		}
 	});
-	
-	if (duplicates>0) {
-		$('#txt').append('Doublons :'+duplicates);
-	}
-	
-	$('#txt').append('<p>Résultat</p>');
+	$(xmlSource).find('allsms').attr('count',total);
 	var blob = new Blob([XMLToString(xmlSource)], {type: "text/xml;charset=utf-8"});
 	saveAs(blob, "test.xml");
+}
+/* Source : http://www.maximechaillou.com/simple-upload-en-drag-and-drop-avec-html5-jquery-php/ */
+function upload(files) {
+	var f = files[0] ;
+	var reader = new FileReader();
+
+	// When the image is loaded,
+	// run handleReaderLoad function
+	reader.onload = handleReaderLoad;
+
+	// Read in the image file as a data URL.
+	reader.readAsText(f);            
+}
+function handleReaderLoad(evt) {
+	var xml = evt.target.result
+	deduplicateSMS($.parseXML(xml));
 }
 
 /* Source : http://www.dotnet-tricks.com/Tutorial/javascript/Y7Q9130612-Convert-string-to-xml-and-xml-to-string-using-javascript.html*/
